dd7bea4d11c1d17b00079c7f603b40afc5cc8ea4
Sandeep Dawre <sandy.dawre123@gmail.com>
2020-10-31 12:53:18 +0530

    created repository in the feature branch

---
+++ src/main/java/com/san/repository/StudRepoInterface.java
@@ -0,0 +1,9 @@
+package com.san.repository;
+
+import com.san.Student;
+
+public interface StudRepoInterface {
+
+    public boolean addStudent(Student student);
+    public boolean delStudent(Student student);
+}
---
+++ src/main/java/com/san/repository/StudRepository.java
@@ -0,0 +1,14 @@
+package com.san.repository;
+
+import com.san.Student;
+
+public class StudRepository implements  StudRepoInterface{
+
+    public boolean addStudent(Student student) {
+        return false;
+    }
+
+    public boolean delStudent(Student student) {
+        return false;
+    }
+}
