b570143ed4f5486fecdff1b5ff6762a228e42bb7
vikas dhongade <vikas.dhongade@gmail.com>
2020-10-31 17:05:23 +0530

    Added security2 class

---
+++ src/main/java/com/san/security/Security2.java
@@ -0,0 +1,14 @@
+package com.san.security;
+
+class Security{
+	public String securityName;	
+
+	public Security(){
+		System.out.println("Security loggging ....!!!");
+	}
+	
+	public String getSecurityName(String name){
+		return this.securityName;
+	}
+
+}
