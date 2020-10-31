197dce9af085d0dd60193ba7986174b6a2dac7f0
vikas dhongade <vikas.dhongade@gmail.com>
2020-10-31 16:50:01 +0530

    added getter method in security module

--- src/main/java/com/san/security/Security.java
+++ src/main/java/com/san/security/Security.java
@@ -1,5 +1,12 @@
 class Security{
+	public String securityName;	
+
 	public Security(){
 		System.out.println("Security loggging ....!!!");
 	}
+	
+	public String getSecurityName(String name){
+		return this.securityName;
+	}
+
 }
